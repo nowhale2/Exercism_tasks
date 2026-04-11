cd29de80be13920c079cd317df4f0e62d398135b
nowhale2 <164938633+nowhale2@users.noreply.github.com>
2026-04-11 20:15:09 +0300

    Create Resistor_Color.py

---
+++ task_34/Resistor_Color.py
@@ -0,0 +1,17 @@
+def color_code(color):
+    return colors().index(color)
+  
+def colors():
+    list_colors = [
+        "black",
+        "brown",
+        "red",
+        "orange",
+        "yellow",
+        "green",
+        "blue",
+        "violet",
+        "grey",
+        "white",
+    ]
+    return list_colors
